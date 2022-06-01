7714a193384ac839e5b4cd54e93a744d83a9eb86
TakutoOno <t-ono@i-enter.co.jp>
2022-06-02 08:53:55 +0900

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by 小野 拓人 on 2022/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
